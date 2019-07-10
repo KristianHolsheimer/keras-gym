3dbc05f789cd11b4b11a447fd276325f2051f366
Kristian <kristian.holsheimer@gmail.com>
2019-07-10 20:19:16 +1000

    added workaround for abseil's tampering with native logging

--- keras_gym/base/mixins.py
+++ keras_gym/base/mixins.py
@@ -8,6 +8,35 @@ from .errors import NonDiscreteActionSpace
 
 
 class LoggerMixin:
+    try:
+        """
+        This workaround was taken from here:
+
+            https://github.com/dhalperi/pybatfish/blob/f8ddd3938148f9a5d9c14c371a099802c564fac3/pybatfish/client/capirca.py#L33-L50
+
+        As of version 1.14, Tensorflow uses Google's abseil-py library, which
+        uses a Google-specific wrapper for logging. That wrapper will write a
+        warning to sys.stderr if the Google command-line flags library has
+        not been initialized.
+
+            https://github.com/abseil/abseil-py/blob/pypi-v0.7.1/absl/logging/__init__.py#L819-L825
+
+        This is not right behavior for Python code that is invoked outside of
+        a Google-authored main program. Use knowledge of abseil-py to disable
+        that warning; ignore and continue if something goes wrong.
+
+        """  # noqa: E501
+        import absl.logging
+
+        # https://github.com/abseil/abseil-py/issues/99
+        logging.root.removeHandler(absl.logging._absl_handler)
+
+        # https://github.com/abseil/abseil-py/issues/102
+        absl.logging._warn_preinit_stderr = False
+
+    except Exception:
+        pass
+
     @property
     def logger(self):
         return logging.getLogger(self.__class__.__name__)
