3c6571626557d31e878929470826c2768c7b915a
Kristian <kristian.holsheimer@gmail.com>
2019-07-10 20:21:13 +1000

    updated alphazero script

--- scripts/connect_four/alphazero.py
+++ scripts/connect_four/alphazero.py
@@ -1,8 +1,15 @@
+import logging
+
 import keras_gym as km
 import numpy as np
 
 
+logging.basicConfig(level=logging.INFO)
+
+
 env = km.envs.ConnectFourEnv()
+env = km.wrappers.TrainMonitor(env)
+
 func = km.predefined.ConnectFourFunctionApproximator(env, lr=0.001)
 ac = km.ConjointActorCritic(func, update_strategy='cross_entropy')
 cache = km.caching.MonteCarloCache(env, gamma=1)
@@ -19,10 +26,10 @@ n = km.planning.MCTSNode(ac, random_seed=17, c_puct=3.5)
 n.env.render()
 
 
-for _ in range(1000):
+for ep in range(1000):
     n.reset()
     for t in range(env.max_time_steps):
-        n.search(n=56)
+        n.search(n=14)
         n.show(2)
         s, pi, r, done = n.play(tau=0)
         cache.add(s, pi, r, done)
@@ -36,5 +43,12 @@ for _ in range(1000):
                 ac.batch_update(S, P, G, In, S_next)
                 G = -G  # flip sign for opponent
 
-            print("*** GAME OVER ***")
+            print("\n*** GAME OVER ***\n\n")
             break
+
+    # store model weights
+    if ep % 10 == 0:
+        ac.train_model.save_weights('train_model.h5')
+        ac.policy.predict_model.save_weights('policy.predict_model.h5')
+        ac.value_function.predict_model.save_weights(
+            'value_function.predict_model.h5')
